e817e3e6b5de463bf74bb8dce2b2b1ea6e54723f
Eamon <eamon.r.burns@gmail.com>
2023-12-15 23:04:56 -0800

    Add simple commands

--- bot.py
+++ bot.py
@@ -7,3 +7,13 @@ load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, '`hello human. what is your request?`')
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
